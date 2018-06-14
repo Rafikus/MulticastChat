d77ebda3d90273e683eb189466eafd0a1b53909f
Rafikus <Rafikus95@gmail.com>
2018-06-14 13:23:29 +0200

    Niepelny szkielet aplikacji

---
+++ ZaliczenieProgramowanieSieciowe/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ZaliczenieProgramowanieSieciowe.Windows
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            UsernameTextBox.Text = e.ToString();
+            Button_Click(sender, null);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow("Kek");
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ ZaliczenieProgramowanieSieciowe/Windows/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ZaliczenieProgramowanieSieciowe.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow(string username)
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
